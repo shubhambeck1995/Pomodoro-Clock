3d5f920dddc9d70014946e4e9bbdc5f85b8b85f0
shubhambeck1995 <shubhambeck1995@gmail.com>
2017-03-29 17:47:23 +0530

    add functionality

--- script.js
+++ script.js
@@ -7,7 +7,9 @@ $(document).ready(function() {
         $("#break-length").html(breakLength);
     });
     $("#dec-break").on("click", function() {
-        breakLength--;
+        if (breakLength === 1) {
+            breakLength = 1;
+        } else breakLength--;
         $("#break-length").html(breakLength);
     });
 
@@ -18,15 +20,74 @@ $(document).ready(function() {
         $("#time-left").html(sessionLength + " :00");
     });
     $("#dec-session").on("click", function() {
-
-        sessionLength--;
+        if (sessionLength == 1) {
+            sessionLength = 1;
+        } else sessionLength--;
         $("#session-length").html(sessionLength);
         $("#time-left").html(sessionLength + " :00");
     });
+var intervalFunction,isRunning;
+    $("#start").on("click", function() {
+if(isRunning){
+  return;
+}
+        var seconds = 60;
+        var breakMinutes = breakLength - 1;
+        var sessionMinutes = sessionLength - 1;
+        var current = "work";
+        var time = sessionMinutes;
+
+        intervalFunction = setInterval(startTimer, 100);
+
+        function startTimer() {
+
+            $("#time-left").html(time + " : " + seconds);
+
+            if (current == "work") {
+                $("#status").html("Work.");
+            } else if (current == "free") {
+                $("#status").html("Break!");
+
+            }
+
+            if (seconds === 0 && current == "work" && time === 0) {
+
+                time = breakMinutes;
+                current = "free";
+                seconds = 60;
+            } else if (seconds === 0 && current == "free" && time === 0) {
+                time = sessionMinutes;
+                current = "work";
+                seconds = 60;
+            }
+            if (seconds === 0 && time > 0) {
+                seconds = 60;
+                time--;
+            }
+            seconds--;
+isRunning=true;
+
+        }
 
 
-    console.log(breakLength);
-    console.log(sessionLength);
+  $("#stop").on("click", function() {
+      clearInterval(intervalFunction);
+      isRunning=false;
+      console.log("stopped");
+      breakLength = 5;
+      sessionLength = 25;
+// $("#break-length").html(breakLength);
+$("#time-left").html(sessionLength+ " :00");
+$("#session-length").html(sessionLength);
+  $("#break-length").html(breakLength);
+    $("#status").html("Work");
+
+  });
+
+
+    });
+
+
 
 
 });
